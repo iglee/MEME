794d135d99dba0eb28c3b040a7a356ba6cb809f2
Isabelle <iglee@uw.edu>
2020-11-08 14:45:28 -0800

    added count matrix function

--- src/meme.py
+++ src/meme.py
@@ -3,12 +3,14 @@ import numpy as np
 import argparse
 from collections import defaultdict
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--str_input", action="store_true", help = "indicates string inputs for comparison")
-parser.add_argument("--file-input", action="store_true", help="indicates file inputs for comparison")
-parser.add_argument("--trainf", action="store_true", help="input train file name")
-parser.add_argument("--testf", action="store_true", help="input test file name")
-args = parser.parse_args()
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--str_input", action="store_true", help = "indicates string inputs for comparison")
+#parser.add_argument("--file-input", action="store_true", help="indicates file inputs for comparison")
+#parser.add_argument("--trainf", action="store_true", help="input train file name")
+#parser.add_argument("--testf", action="store_true", help="input test file name")
+#args = parser.parse_args()
+
+MAXLEN = 113
 
 def read_data(filename):
     """
@@ -21,24 +23,45 @@ def read_data(filename):
 
     for i in range(0,len(lines),3):
         ident = lines[i].strip()
-        seq = lines[i+1].strip() + lines[i+2].strip()
+        seq = lines[i+1].strip().upper() + lines[i+2].strip().upper()
         d[ident] = list(seq)
 
     return d
 
 
-if args.file:
-    Dtrain = read_data(args.trainf)
-    Dtest = read_data(args.testf)
-    None
+#if args.file:
+#    Dtrain = read_data(args.trainf)
+#    Dtest = read_data(args.testf)
+    
 
-if args.str_input:
-    None
+#if args.str_input:
+#    None
 
 
 
-def makeCountMatrix():
-    return None
+def makeCountMatrix(data_dict):
+    vec_a = np.zeros(MAXLEN)
+    vec_t = np.zeros(MAXLEN)
+    vec_c = np.zeros(MAXLEN)
+    vec_g = np.zeros(MAXLEN)
+
+    for i in range(MAXLEN):
+        a, t, c, g = 0, 0, 0, 0
+        for seq in data_dict.values():
+            try:
+                if seq[i] == "A":
+                    a += 1
+                if seq[i] == "T":
+                    t += 1
+                if seq[i] == "C":
+                    c += 1
+                if seq[i] == "G":
+                    g += 1
+            except:
+                pass
+            vec_a[i], vec_t[i], vec_c[i], vec_g[i] = a, t, c, g
+
+    return np.stack((vec_a, vec_t, vec_c, vec_g))
 
 def addPseudo():
     return None
