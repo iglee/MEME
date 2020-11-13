20c4e3aaab12516c3de5a4d3c422faa06a75cb8c
Isabelle <iglee@uw.edu>
2020-11-12 22:43:46 -0800

    roc curves for both train and test

--- src/meme.py
+++ src/meme.py
@@ -188,29 +188,50 @@ final_idx = entropies.argmax()
 D, D_freq = motif_wmms[final_idx], freq_matrices[final_idx]
 #print(D)
 
-score_D = []
-y_true = []
-positions = []
 
-# scores based on D
-for x in test_seqs:
-    scores = scanWMM(x, D)
-    
-    positions.append(scores.argmax())
-    score_D.append(scores)
-    y_seq = np.zeros(len(scores))
-    y_seq[46:58] = 1
-    y_true.append(y_seq)
-    
 
-#print(np.concatenate(score_D))
-#print(np.concatenate(y_true))
-fpr, tpr, t = roc_curve(np.concatenate(y_true), np.concatenate(score_D))
-roc_auc = auc(fpr, tpr)
+####################################################
+#             PLOT HISTOGRAM AND ROC               #
+####################################################
+
+
+def format_results(test_seqs, wmm):
+    score_D = []
+    y_true = []
+    positions = []
+
+    # scores based on D
+    for x in test_seqs:
+        scores = scanWMM(x, wmm)
+        
+        positions.append(scores.argmax())
+        score_D.append(scores)
+        y_seq = np.zeros(len(scores))
+        y_seq[46:58] = 1
+        y_true.append(y_seq)
+        
+    return roc_curve(np.concatenate(y_true), np.concatenate(score_D))
+
+# test sequences
+test_fpr, test_tpr, test_t = format_results(test_seqs, D)
+test_roc_auc = auc(test_fpr, test_tpr)
+
+# train sequences
+train_fpr, train_tpr, train_t = format_results(seqs, D)
+train_roc_auc = auc(train_fpr, train_tpr)
+
+# debug test sequences
+#test_fpr, test_tpr, test_t = format_results(test_seqs, D)
+#test_roc_auc = auc(test_fpr, test_tpr)
+
+# debug train sequences
+#test_fpr, test_tpr, test_t = format_results(test_seqs, D)
+#test_roc_auc = auc(test_fpr, test_tpr)
 
 plt.figure()
 lw = 2
-plt.plot(fpr, tpr, color='darkorange', lw=lw)
+plt.plot(test_fpr, test_tpr, color='darkorange', lw=lw)
+plt.plot(train_fpr, train_tpr, color='red', lw=lw)
 plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
